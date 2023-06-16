03a19af2d95cde6a9e02cdddf65c9a77e9500a48
Aleksey Yesin <esinru@gmail.com>
2023-06-16 13:04:53 +0300

    Config sass paths

--- src/app/abc/abc.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {};
-
-const Abc = (props: Props) => {
-  return <div>abc</div>;
-};
-
-export default Abc;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import css from "./page.module.scss";
-// import Abc from "/src/app/abc/abc";
 
 export default function Home() {
-  return (
-    <main>
-      Home Page
-      {/* <Abc /> */}
-    </main>
-  );
+  return <main>Home Page</main>;
 }
